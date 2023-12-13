ade2a212b20f8074de5e57f328e8ea59c34f29c0
nadiamalik7077 <nadiamalik.7077@gmail.com>
2023-12-12 20:55:50 -0800

    Initial commit

---
+++ app/src/main/java/com/example/loanturbo/ui/home/HomeFragment.java
@@ -0,0 +1,42 @@
+package com.example.loanturbo.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.loanturbo.R;
+import com.example.loanturbo.databinding.FragmentHomeBinding;
+import com.github.hamzaahmedkhan.counteranimationtextview.CountAnimationTextView;
+import com.shuhart.stepview.StepView;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+    CountAnimationTextView counter;
+    StepView stepView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        counter= root.findViewById(R.id.user_counter);
+        stepView= root.findViewById(R.id.step_view);
+
+        counter.setAnimationDuration(5000).countAnimation(0, 567996599);
+        stepView.go(2,true);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
